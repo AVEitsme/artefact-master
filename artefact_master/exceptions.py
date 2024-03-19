8daf9dcd11630b438a7e75020c9d63d9ff7f4088
aveitsme <aveitsme@gmail.com>
2024-03-19 23:15:02 +0300

    ArtefactNotFoundException added

--- artefact_master/exceptions.py
+++ artefact_master/exceptions.py
@@ -1,15 +1,17 @@
-from artefact_master.aretefacts import Artefact
-
 from typing import Dict
+
+from artefact_master.aretefacts import Artefact
 
 
 class NotRegisteredError(Exception):
     """Raised when an artefact doesn't exists in the registered artefacts."""
 
-    def __init__(self, artefact_name: str, registered_artefacts: Dict[str, Artefact]) -> None:
+    def __init__(
+        self, artefact_name: str, registered_artefacts: Dict[str, Artefact]
+    ) -> None:
         self.artefact_name = artefact_name
         self.registered_artefacts = registered_artefacts
-        self.message = f"Artefact {artefact_name} doesn't exists in registered artefacts: {registered_artefacts}"
+        self.message = f"Artefact {artefact_name} doesn't exists in registered artefacts: {registered_artefacts}."
 
 
 class AlreadyRegisteredError(Exception):
@@ -17,7 +19,14 @@ class AlreadyRegisteredError(Exception):
 
     def __init__(self, artefact_name: str) -> None:
         self.artefact_name = artefact_name
-        self.message = f"Artefact {artefact_name} already exists in registered artefacts." 
+        self.message = (
+            f"Artefact {artefact_name} already exists in registered artefacts."
+        )
 
 
-# ArtefactNotFoundError
+class ArtefactNotFoundError(Exception):
+    """Raised when an artefact doesn't exists in the filesystem."""
+
+    def __init__(self, artefact_path: str) -> None:
+        self.artefact_path = artefact_path
+        self.message = f"Artefact doesn't exists at given path: {artefact_path}."
